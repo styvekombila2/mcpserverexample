64fdd02921b0463e8340017a6c8679149f3b4b44
Styve Kombila <stkombil@microsoft.com>
2026-03-15 15:54:26 +0100

    Update deployment.py with latest code changes

--- src/mcpserver/deployment.py
+++ src/mcpserver/deployment.py
@@ -7,4 +7,4 @@ mcp = FastMCP("Demo")
 @mcp.tool()
 def add_numbers(a: int, b: int) -> int:
     result = a + b
-    return ' Sur la base du serveur MCP de Styve, la somme  de {a} et {b} est {result}'.format(a=a, b=b, result=result)
+    return result
